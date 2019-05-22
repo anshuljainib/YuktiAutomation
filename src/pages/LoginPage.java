8aa788e5a9c9492ad9c6e7b8dc8f5e668360f0bd
anshuljainib <anshula.jain@infobeans.com>
2019-05-22 16:06:28 +0530

    Commiting initial framework design

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import org.eclipse.jetty.util.annotation.Name;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	//Page Factory - OR:
+	@FindBy(name="username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//button[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="//img[contains(@class,'img-responsive')]")
+	WebElement crmLogo;
+	
+	//Initializing the Page Objects:
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle(){
+		return driver.getTitle();
+	}
+	
+	public boolean validateCRMImage(){
+		return crmLogo.isDisplayed();
+	}
+	
+	public login(String un, String pwd){
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		loginBtn.click();
+		    	
+		    	
+		    
+		
+		return new HomePage();
+	}
+	
+
+}
